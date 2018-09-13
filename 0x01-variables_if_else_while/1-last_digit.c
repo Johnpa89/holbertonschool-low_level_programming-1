a1a61b5c0902abf3f140fa78c4db42c86c8bf961
Derrick Gee <km.commissions@gmail.com>
2018-09-13 21:19:06 +0000

    fixed variables and order of if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int L;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n%10;
+	L = n % 10;
 
-	if (n > 5)
-		printf("The last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	else if (n < 6)
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-	else
-		printf("The last digit of %d is %d and is 0\n", n, lastDigit);
+	if (L > 5)
+		printf("The last digit of %d is %d and is greater than 5\n", n, L);
+	else if (L == 0)
+		printf("The last digit of %d is %d and is 0\n", n, L);
+	else if (L < 6)
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, L);
 
 	return (0);
 }

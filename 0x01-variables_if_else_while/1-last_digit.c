324b2c92fc3da42fb8007372be64b085bf1e10cc
Derrick Gee <km.commissions@gmail.com>
2018-09-13 21:09:19 +0000

    changed if order and made lastdigit an int

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,14 +9,18 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastDigit = n%10;
+
 	if (n > 5)
-		printf("The last digit of %d is %d and is greater than 5\n", n, n%10);
-	else if (n == 0)
-		printf("The last digit of %d is %d and is 0\n", n, n%10);
+		printf("The last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	else if (n < 6)
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	else
+		printf("The last digit of %d is %d and is 0\n", n, lastDigit);
+
 	return (0);
 }

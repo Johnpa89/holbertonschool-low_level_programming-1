f344a0b8c2ac629c15930513825de6ee0b0753f1
Derrick Gee <km.commissions@gmail.com>
2018-09-13 21:23:58 +0000

    added a char for string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,17 +10,18 @@ int main(void)
 {
 	int n;
 	int L;
+	char str[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	L = n % 10;
 
 	if (L > 5)
-		printf("The last digit of %d is %d and is greater than 5\n", n, L);
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
 	else if (L == 0)
-		printf("The last digit of %d is %d and is 0\n", n, L);
+		printf("%s %d is %d and is 0\n", str, n, L);
 	else if (L < 6)
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, L);
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 
 	return (0);
 }

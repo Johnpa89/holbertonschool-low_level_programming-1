780fae3cc367857519857fff2e1aa6ea0ca16b5f
Derrick Gee <km.commissions@gmail.com>
2018-09-13 20:58:29 +0000

    fixed else and added newlines

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero", n);
-	else (n < 0)
-		printf("%d is negative", n);
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
 }

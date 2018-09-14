323690abcceaae04fadbc714c5f2b3a33060f0eb
Derrick Gee <km.commissions@gmail.com>
2018-09-14 18:50:03 +0000

    testing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 	return (1);
 }

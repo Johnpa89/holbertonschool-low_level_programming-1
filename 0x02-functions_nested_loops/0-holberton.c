9824f49f00c439048c59d0f22bd9a4d3798d3c29
Derrick Gee <km.commissions@gmail.com>
2018-09-18 21:34:49 +0000

    changed to while loop

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char *sh = "Holberton\n";
-	int i;
+	char *sh = "Holberton";
 
-	for (i = 0; i <= 11; i++)
+	while (*sh)
 	{
 		_putchar(*sh);
 		sh++;
 	}
+	_putchar('\n');
 
 	return (0);
 }

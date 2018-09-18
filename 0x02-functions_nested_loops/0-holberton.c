d20a85af0ce1c7474ad60b435902c0e30062b63d
Derrick Gee <km.commissions@gmail.com>
2018-09-18 20:54:09 +0000

    testing loop

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,16 +6,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char *sh = "Holberton\n";
+	int i;
+
+	for (i = 0; i <= 11; i++)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 
 	return (0);
 }

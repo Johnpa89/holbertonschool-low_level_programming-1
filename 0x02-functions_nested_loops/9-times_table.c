cab55d7c355c3ba99f2b1021c128fd1bba1174b5
Derrick Gee <km.commissions@gmail.com>
2018-09-19 04:24:29 +0000

    #9 local test successful

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * rone = row, cone = column, d = digits of current result
+ * Return: Always 0 (Success)
+ * add extra space past single digit
+ */
+void times_table(void)
+{
+	int rone, cone, d;
+
+	for (rone = 0; rone <= 9; rone++)
+	{
+		for (cone = 0; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) != 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}

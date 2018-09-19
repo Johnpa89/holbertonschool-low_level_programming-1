0c45d0caf1cb463acad8d5ad8dbedafd419f0e56
Derrick Gee <km.commissions@gmail.com>
2018-09-19 19:45:26 +0000

    fixed over 80 char betty error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return: x10 a-z
  */

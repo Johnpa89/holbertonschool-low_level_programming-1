4455b91e4dcd13893ee9881b06e104786d6d6f7e
Derrick Gee <km.commissions@gmail.com>
2018-09-21 01:44:39 +0000

    updated header file & saving adv

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints prime factors of 612852475143
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_number - a function that prints an integer
+ * @n: input
+ * Return: inputted integer
+ */
+void print_number(int n)
+{
+
+	return (0);
+}

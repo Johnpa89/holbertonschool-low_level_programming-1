d4dbf55701fc2e1f4b0b824c29e53a76fcc34201
Derrick Gee <km.commissions@gmail.com>
2018-09-21 01:24:51 +0000

    removed space after 100

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,9 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s ", fb);
 		else if (i % 3 == 0)
 			printf("%s ", f);

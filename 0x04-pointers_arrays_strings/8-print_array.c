12cbc8eadbac4a96221cc975015ad3446c90ab9d
Derrick Gee <km.commissions@gmail.com>
2018-09-26 15:58:52 +0000

    removed hard code for commas

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -13,6 +13,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 		printf("%d, ", a[i]);
-	printf("%d", a[n - 1]);
+		if (i == (n - 1))
+			printf("%d", a[n - 1]);
 	printf("\n");
 }

b63120933b3502e05e12a1f91fb61591d53fad2b
Derrick Gee <km.commissions@gmail.com>
2018-09-28 06:18:40 +0000

    added parameters

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,9 +7,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *n);
+char *cap_string(char *n);
+char *leet(char *n);
+char *rot13(char *n);
 
 #endif /* HOLBERTON_H */

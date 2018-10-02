aca566a558f22a60dbbb9b698407f008f3bfc3be
Derrick Gee <km.commissions@gmail.com>
2018-10-02 03:36:42 +0000

    added 2

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -8,6 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
+	int i;
 
-	return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
 }

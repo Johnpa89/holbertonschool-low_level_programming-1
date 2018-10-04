dac15263c15a240f75ac909ae43f4147caa08b67
kai-dg <km.commissions@gmail.com>
2018-10-04 04:27:24 +0000

    2nd test

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -8,10 +8,9 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	else
+		_putchar('\n');
+
+	_putchar(*s);
 }

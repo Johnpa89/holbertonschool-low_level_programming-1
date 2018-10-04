bbcaba552225d031ed67b18e9428769c8d9a9bae
kai-dg <km.commissions@gmail.com>
2018-10-04 08:47:06 +0000

    locally tested

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+int squareroot(int n, int i);
+/**
+ * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+        	return(-1);
+    	else
+        	return (squareroot(n, (n + 1) / 2));
+}
+
+int squareroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+        	return (i);
+	else
+        	return (squareroot(n, i - 1));
+}

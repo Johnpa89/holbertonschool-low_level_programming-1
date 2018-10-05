22094a2cb2a0167ffa71a3d41d051a2ed2f308ae
kai-dg <km.commissions@gmail.com>
2018-10-05 06:18:37 +0000

    fixed betty doc and style

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -8,17 +8,22 @@ int squareroot(int n, int i);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-        	return(-1);
-    	else
-        	return (squareroot(n, (n + 1) / 2));
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
 }
-
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
 int squareroot(int n, int i)
 {
 	if (i < 1)
 		return (-1);
 	else if (i * i == n)
-        	return (i);
+		return (i);
 	else
-        	return (squareroot(n, i - 1));
+		return (squareroot(n, i - 1));
 }

993324b371a4279399ae4a78d15473027fa51353
kai-dg <km.commissions@gmail.com>
2018-10-10 05:59:23 +0000

    tested locally

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 /**
- * main - Entry Point
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
  * Return: 0
  */
-int main(void)
+char *argstostr(int ac, char **av)
 {
-	return (0);
+	int i, n, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
+
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+	return (str);
 }

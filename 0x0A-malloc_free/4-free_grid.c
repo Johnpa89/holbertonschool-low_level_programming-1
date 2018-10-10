d41301f0b51407e30ca46e58966863074897b1db
kai-dg <km.commissions@gmail.com>
2018-10-10 04:44:55 +0000

    combined statements

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -11,14 +11,12 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height == 0)
-	{
-		return;
-	}
-	else
+	if (grid != NULL || height != 0)
 	{
 		for (i = 0; i < height; i++)
+		{
 			free(grid[i]);
+		}
+		free(grid);
 	}
-	free(grid);
 }

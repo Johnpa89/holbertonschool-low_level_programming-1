9c5ff8879a8109ca2bc44fe5bbd4bebf531558da
kai-dg <km.commissions@gmail.com>
2018-10-12 01:36:03 +0000

    locally tested, initial commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}
---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * array_range - get len, malloc * len
+ * loop size of len to insert min++ into new arr
+ * @min: min input
+ * @max: max input
+ * Return: pointer to new arr of ints
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, len;
+
+	if (min > max)
+		return (NULL);
+
+	for (len = 0; len <= (max - min); len++)
+	{
+	}
+
+	arr = malloc(sizeof(int) * len);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		arr[i] = min;
+		min++;
+	}
+
+	return (arr);
+}

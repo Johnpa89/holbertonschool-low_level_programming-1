41650267ca5d6833b931c1e56cab2c1ad98859ee
kai-dg <km.commissions@gmail.com>
2018-10-12 20:21:43 +0000

    fixed bytes allocated

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -16,11 +16,10 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	for (len = 0; len <= (max - min); len++)
-	{
-	}
+	for (len = 0; len < (max - min); len++)
+		;
 
-	arr = malloc(sizeof(int *) * len);
+	arr = malloc(sizeof(int) * (len + 1));
 	if (arr == NULL)
 		return (NULL);
 

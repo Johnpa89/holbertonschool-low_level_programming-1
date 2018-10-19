8c6ae8478249108e3aa86bdcfc5af50996815aa1
kai-dg <km.commissions@gmail.com>
2018-10-19 06:10:13 +0000

    fixed NULL edge case

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -14,6 +14,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL)
 		return;
+	if (action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 		action(array[i]);

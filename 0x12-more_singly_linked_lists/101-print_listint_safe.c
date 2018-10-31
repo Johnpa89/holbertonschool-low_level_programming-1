60f6c952ee5ad6b27a70c346f25c7d4db7944e2d
kai-dg <km.commissions@gmail.com>
2018-10-31 06:03:48 +0000

    adding #12

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * print_listint_safe - Entry Point
+ * @head: head
+ * Return: 0
+ */
+size_t print_listint_safe(const listint_t *head);
+{
+	if (head == NULL)
+		exit(98);
+
+	return (0);
+}

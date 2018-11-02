6e9c081cd11c0b95ff8b3acf0a749bdc5b207069
kai-dg <km.commissions@gmail.com>
2018-11-02 05:49:18 +0000

    changed to unsigned int, added else condition

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -5,11 +5,11 @@
  */
 int get_endianness(void)
 {
-	int i = 1;
-	char *c = (char *) &i;
+	unsigned int i = 1;
+	char *c = (char *)&i;
 
 	if (*c)
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
